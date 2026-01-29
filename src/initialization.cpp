3209ac967e05e689208bea4f8f547b97855d808c
riad2008004 <arifulislamriad5@gmail.com>
2026-01-30 02:50:45 +0600

    speed updated

--- src/initialization.cpp
+++ src/initialization.cpp
@@ -1,17 +1,17 @@
 #include <Arduino.h>
 #include "initialization.h"
 
-const int BASE_SPEED_HIGH = 80;
+const int BASE_SPEED_HIGH = 100;
 const int BASE_SPEED_LOW = 20;
 const int ACTUATOR_1_SPEED_HIGH = 250;
 const int ACTUATOR_1_SPEED_LOW = 50;
 const int ACTUATOR_2_SPEED_HIGH = 250;
 const int ACTUATOR_2_SPEED_LOW = 50;
-const int WRIST_SPEED_HIGH = 80;
+const int WRIST_SPEED_HIGH = 100;
 const int WRIST_SPEED_LOW = 20;
-const int ROTATION_360_SPEED_HIGH = 80;
+const int ROTATION_360_SPEED_HIGH = 100;
 const int ROTATION_360_SPEED_LOW = 20;
-const int GRIPPER_SPEED_HIGH = 150;
+const int GRIPPER_SPEED_HIGH = 200;
 const int GRIPPER_SPEED_LOW = 50;
 
 void arm_pin_config()
